c56bcbd52daf30a0c1e1a5966a429b0c6b4a511f
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2023-02-03 18:51:24 +0000

    Stream the reply in chunks

--- src/cogs/chat_gpt.py
+++ src/cogs/chat_gpt.py
@@ -3,6 +3,8 @@ import openai
 import os
 from guesslang import Guess
 
+CHUNK_SIZE = 15
+
 openai.api_key = os.getenv("OPEN_AI_TOKEN")
 
 
@@ -10,6 +12,7 @@ class ChatGpt(commands.Cog):
 
 	def __init__(self, client):
 		self.client = client
+		self.chunk_size = CHUNK_SIZE
 		self.guess = Guess()
 
 	@commands.Cog.listener()
@@ -18,18 +21,34 @@ class ChatGpt(commands.Cog):
 
 	@commands.command()
 	async def ask(self, ctx, *, prompt):
-		async with ctx.typing():
-			response = openai.Completion.create(
-			    engine="text-davinci-003",
-			    prompt=prompt,
-			    max_tokens=2048,
-			    n=1,
-			    stop=None,
-			    temperature=0.5).choices[0].text
+		# Place holder for the reply.
+		sent_message = await ctx.send("Processing your request...")
 
-			language = self.guess.language_name(response)
+		# Get the ChatGPT generator.
+		completions = openai.Completion.create(engine="text-davinci-003",
+		                                       prompt=prompt,
+		                                       max_tokens=2048,
+		                                       n=1,
+		                                       stop=None,
+		                                       temperature=0.5,
+		                                       stream=True)
 
-			await ctx.send("```{}\n".format(language) + response + "\n```")
+		# Build reply from the stream of chunks
+		reply = ""
+		for chunk in completions:
+			reply += chunk["choices"][0]["text"]
+			# Update the message every CHUNK_SIZE chunks to avoid Discord's API rate limit.
+			if len(reply) % self.chunk_size == 0:
+				await sent_message.edit(content=reply)
+
+		# Programming languages get a code block.
+		language = self.guess.language_name(reply)
+		if language != None:
+			await sent_message.edit(content="```{}".format(language) + reply +
+			                        "```")
+		# Update the message with the last chunk.
+		else:
+			await sent_message.edit(content=reply)
 
 
 async def setup(client):
