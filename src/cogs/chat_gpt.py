b0391d18078b6d2b3850548ef0c3593f0d225d56
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2023-03-04 22:38:19 +0000

    feat: replace guesslang with pygments

--- src/cogs/chat_gpt.py
+++ src/cogs/chat_gpt.py
@@ -1,10 +1,9 @@
 from discord.ext import commands
 import openai
 import os
-from guesslang import Guess
+from pygments.lexers import guess_lexer
 
 CHUNK_SIZE = 15
-PROB_THRESHOLD = 0.5
 
 openai.api_key = os.getenv("OPEN_AI_TOKEN")
 
@@ -14,8 +13,7 @@ class ChatGpt(commands.Cog):
 	def __init__(self, client):
 		self.client = client
 		self.chunk_size = CHUNK_SIZE
-		self.guess = Guess()
-		self.prob_threshold = PROB_THRESHOLD
+		self.guess = guess_lexer
 
 	@commands.Cog.listener()
 	async def on_ready(self):
@@ -45,11 +43,9 @@ class ChatGpt(commands.Cog):
 				await sent_message.edit(content=reply)
 
 		# Programming languages get a code block.
-		probabilities = self.guess.probabilities(reply)
-		if probabilities[0][1] > self.prob_threshold:
-			await sent_message.edit(
-			    content="```{}".format(probabilities[0][0]) + reply + "```")
-		# Update the message with the last chunk.
+		lang = self.guess(reply).name.lower()
+		if lang != "text only":
+			await sent_message.edit(content="```{}\n".format(lang) + reply + "\n```")
 		else:
 			await sent_message.edit(content=reply)
 
